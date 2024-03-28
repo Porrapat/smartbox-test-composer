ba725afced220a95ac31f9414d7292a4598fbe3c
Porrapat Petchdamrongskul <porrapat@webgatosoftware.com>
2024-03-28 15:50:58 +0700

    Add more tests1.php file to test with socket.io

---
+++ tests1.php
@@ -0,0 +1,34 @@
+<?php
+require 'vendor/autoload.php';
+
+use ElephantIO\Client;
+
+$url = 'http://localhost:8081';
+
+// if client option is omitted then it will use latest client available,
+// aka. version 4.x
+$options = ['client' => Client::CLIENT_4X];
+
+$client = Client::create($url, $options);
+$client->connect();
+$client->of('/'); // can be omitted if connecting to default namespace
+
+// emit an event to the server
+$data = ['username' => 'my-user'];
+$client->emit('get-user-info', $data);
+
+// wait an event to arrive
+// beware when waiting for response from server, the script may be killed if
+// PHP max_execution_time is reached
+if ($packet = $client->wait('user-info')) {
+    // an event has been received, the result will be a \ElephantIO\Engine\Packet class
+    // data property contains the first argument
+    // args property contains array of arguments, [$data, ...]
+    $data = $packet->data;
+    $args = $packet->args;
+    // access data
+    $email = $data['email'];
+}
+
+// end session
+$client->disconnect();
