e4b1a005ce6e876cd2a3e3d41023575f7492c723
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2024-04-04 16:40:20 +0700

    Finished index3 stage 8443

--- index3.js
+++ index3.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs'); // Required to read SSL certificate files
+const { Server } = require('socket.io');
+const { join } = require('node:path');
 
 const app = express();
 const port = 8443;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.sendFile(join(__dirname, 'index3.html'));
+  // res.send('hello world');
 });
 
 // Load SSL certificate and key files
@@ -19,6 +22,26 @@ const options = {
 // Create HTTPS server
 const server = https.createServer(options, app);
 
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('foo', function (data) {
+    console.log('here we are in action event and data is: ' + data);
+  });
+  socket.on('get-user-info', (data) => {
+    console.log('get-user-info');
+    console.log(data);
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat_broadcast_message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+  
 server.listen(port, () => {
   console.log(`Example app listening on port ${port} over HTTPS`);
 });
